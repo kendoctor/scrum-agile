1dc8b1036f915b2d46b4ba2f422125d4dd9fd2f3
kendoctor <kendoctor@163.com>
2015-10-25 23:28:56 +0800

    完成FOSUserBundle的配置

---
+++ src/Kendoctor/Bundle/AppBundle/Entity/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Kendoctor\Bundle\AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Kendoctor\Bundle\AppBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
---
+++ src/Kendoctor/Bundle/AppBundle/KendoctorAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kendoctor\Bundle\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KendoctorAppBundle extends Bundle
+{
+}
